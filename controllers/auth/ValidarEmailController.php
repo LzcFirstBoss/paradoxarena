6f320774dc8d40de3275253a406fb95f83e61133
LzcFirstBoss <luyzpim10102@gmail.com>
2025-03-03 00:47:31 -0300

    redirecionando para login validar email

--- controllers/auth/ValidarEmailController.php
+++ controllers/auth/ValidarEmailController.php
@@ -5,6 +5,10 @@ require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../models/auth/UsuarioToken.php';
 require_once __DIR__ . '/../../models/auth/user.php';
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 
 class ValidarEmailController {
     public function validate() {
@@ -18,7 +22,9 @@ class ValidarEmailController {
             if ($tokenData) {
                 // Verifica se o token expirou
                 if ($tokenData['expira_em'] < date('Y-m-d H:i:s')) {
-                    echo "Token expirado. Por favor, solicite um novo link de ativação.";
+                    $_SESSION['erros'] = ["Erro: Token expirado. Por favor, solicite um novo link de ativação."];
+                    header('Location: /paradoxarena/public/login');
+                    exit;
                 } else {
                     // Conecta ao banco e instancia o model de usuário
                     $pdo = (new Database())->connect();
@@ -30,13 +36,19 @@ class ValidarEmailController {
                     // Remove o token utilizado
                     $tokenModel->deleteToken($tokenData['id']);
                     
-                    echo "Seu e-mail foi confirmado com sucesso! Agora você pode acessar sua conta.";
+                    $_SESSION['sucesso'] = "Sucesso: Seu e-mail foi confirmado com sucesso!.";
+                    header('Location: /paradoxarena/public/login');
+                    exit;
                 }
             } else {
-                echo "Token inválido.";
+                $_SESSION['erros'] = ["Erro: token invalido"];
+                header('Location: /paradoxarena/public/login');
+                exit;
             }
         } else {
-            echo "Token não fornecido.";
+            $_SESSION['erros'] = ["Erro: Token não fornecido"];
+            header('Location: /paradoxarena/public/login');
+            exit;
         }
     }
 }
